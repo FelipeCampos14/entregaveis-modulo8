1e7800fd404a3193300449753600f07939cb0e3b
Felipe Campos <Felipe.Campos@sou.inteli.edu.br>
2023-11-24 16:25:36 -0300

    Fix

--- Ponderada3/README.md
+++ Ponderada3/README.md
@@ -1,13 +1,21 @@
-# Como rodar
+# Vídeo
 
+
+
+# Como rodar
+A estrutura escolhida foi a de um workspace do ros, pois inicialmente seria integrado com o robô. Apesar de não ter sido integrado com o robô, a praticidade de dar source no workspace para carregar todas as dependências me manteve utilizando essa estratégia.
+
+Primeiramente é necessário carregar os pacotes:
 ´´´
 colcon build
 ´´´
 
+Agora é necessário dar source para carregar as depedências:
 ´´´
 source install/local_setup.bash
 ´´´
 
+Finalmente para rodar a ponderada, basta rodar o nó:
 ´´´
 ros2 run ponderada3 chat_node
 ´´´
--- Ponderada3/workspace/src/ponderada3/ponderada3/chat2.py
+++ Ponderada3/workspace/src/ponderada3/ponderada3/chat2.py
@@ -4,35 +4,33 @@ import re
 import time
 
 intent_dict = {
-    r"\b(?:(?:[Bb]o[am])\s(?:tarde|dia|noite))": "greetings",
-    r"\b(?:[Vv][aá][i]*|[Dd]ir[i]*ja[-se]*|l[e]*v[e]*|ande).+(?:ao|[àa])?\s\b([\wáéíóúâêîôûãõç]+\b)": "command"
+    r"\b(?:[Vv][aá][i]*|[Dd]ir[i]*ja[-se]*|l[e]*v[e]*|ande).+(?:[àa]?[o]|)?\s\b(lab[a-o-r]+t[óo]rio|lab|s[c-e-r]+[a-r-t]+[a-i-r]+|b[i-l]+b[i-l]+[e-o-t]+ca)\b": "command",
 }
 
 actions = {
-    "biblioteca":"a posição da biblioteca fica em (2.0,3.0)",
-    "secretaria":"a posição da secretaria fica em (1.0,4.0)",
-    "laboratório":"a posição da laboratório fica em (0.0,4.0)"
+    r"(b[i-l]+b[i-l]+[e-o-t]+ca)":"a posição da biblioteca fica em (2.0,3.0)",
+    r"(s[c-e-r]+[a-r-t]+[a-i-r]+)":"a posição da secretaria fica em (1.0,4.0)",
+    r"(lab[a-o-r]+t[óo]rio|lab)":"a posição do laboratório fica em (0.0,4.0)"
 }
 def main():
     while True:
-        command = input("Para onde deseja ir: ")
+        command = input("Como posso te ajudar? ")
         if command == "quit()":
             break
         else:
             for key, value in intent_dict.items():
                 pattern = re.compile(key)
                 groups = pattern.findall(command)
-                confirmacao = input(f"Você deseja ir à {groups[0]}? (yes|no) ")
-                if groups and value=="command" and confirmacao=="yes":
+                if groups and value=="command" :
                     for key_actions in actions.keys():
-                        if key_actions == groups[0]:
-                            print(actions[key_actions][0])
+                        pattern_key_action = re.compile(key_actions)
+                        if pattern_key_action.findall(groups[0].lower()):
+                            print(actions[key_actions])
                             print('Andando...')
                             time.sleep(5)
-                            print("Cheguei!")
-                elif confirmacao =="no":
-                    break
-                
+                            print("Cheguei!\n")
+                else:
+                    print("Não entendi, repita por favor.\n")
 
 
                 
