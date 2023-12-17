18b50676bdea7a3c4cf54be1381e784e60b8c336
Felipe Campos <Felipe.Campos@sou.inteli.edu.br>
2023-12-17 18:55:28 -0300

    Ponderada8 sem vídeo

--- Ponderada8/main.py
+++ Ponderada8/main.py
@@ -11,9 +11,9 @@ def select_language():
     actions = {
         r"(portugu[êe]s|[Pp][Tt])":"pt",
         r"(ingl[êe]s|[Ee][Nn])":"en",
-        r"(japon[êe]s|[Jj][Aa])":"ja"
+        r"([Ff]ranc[eê]s|[Ff][Rr])":"fr"
     }
-    lan = input("Selecione uma língua de sua escolha: (pt/en/ja) \n")
+    lan = input("Selecione uma língua de sua escolha: (pt/en/fr) \n")
     for key, value in actions.items():
             pattern = re.compile(key)
             groups = pattern.findall(lan)
@@ -21,13 +21,13 @@ def select_language():
                 return 'pt'
             elif groups and value =='en':
                 return 'en'
-            elif groups and value =='ja':
-                return 'ja'
+            elif groups and value =='fr':
+                return 'fr'
 
 def select_audio():
-    audio = input("Selecione um audio para transcrever e traduzir: (audio1.mp3/audio2.mp3/audio3.mp3) \n")
+    audio = input("Selecione um audio para transcrever e traduzir: (audio1.mp3/audio2.ogg) \n")
     model = whisper.load_model("base")
-    print('ouvindo...')
+    print('Ouvindo...')
     return model.transcribe(audio)['text']
 
 def tts(language, mytext):
@@ -35,16 +35,23 @@ def tts(language, mytext):
     myobj.save("out.mp3") 
     p = vlc.MediaPlayer("out.mp3")
     p.play()
-    time.sleep(5)
+    time.sleep(4)
 
 
 def main():
-    text = select_audio()
-    lan = select_language()
-    print(lan)
-    translator = GoogleTranslator(source='auto', target=lan).translate(text=text)
-    print(translator)
-    tts(lan, translator)
+    while True:
+        proceed = input("Deseja traduzir algum áudio? \n")
+        if proceed.lower() == "sim":
+            text = select_audio()
+            lan = select_language()
+            translator = GoogleTranslator(source='auto', target=lan).translate(text=text)
+            print(f'Texto traduzido para {lan}: {translator}')
+            tts(lan, translator)
+        elif proceed.lower() == "não" or proceed.lower() == "nao":
+            break
+        else: 
+            print(proceed.lower())
+            print('Desculpa não entendi, repita por favor')
 
 if __name__ == "__main__":
     main()
